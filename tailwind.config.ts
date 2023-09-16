86ee69e0ad23029d13acb05d35503929ff2c95d9
jp calvo <calvojp92@gmail.com>
2023-09-16 11:13:56 +0800

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-import type { Config } from 'tailwindcss'
+import typography from '@tailwindcss/typography';
+import { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +9,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: 'var(--font-sans)',
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [typography],
+};
+
+export default config;
