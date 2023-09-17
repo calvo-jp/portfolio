22aba203a61290c6cf715abc1297ec6bc1b1e896
jp calvo <calvojp92@gmail.com>
2023-09-17 19:06:40 +0800

    add use-disclosure hook

---
+++ src/lib/use-disclosure.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+interface UseDisclosureProps {
+  onOpen?(): void;
+  onClose?(): void;
+  defaultValue?: boolean;
+}
+
+export function useDisclosure(props: UseDisclosureProps = {}) {
+  const [open, setOpen] = useState(!!props.defaultValue);
+
+  const onOpen = () => {
+    setOpen(true);
+    props.onOpen?.();
+  };
+
+  const onClose = () => {
+    setOpen(false);
+    props.onClose?.();
+  };
+
+  const onToggle = () => {
+    setOpen((value) => !value);
+  };
+
+  return {
+    open,
+    onOpen,
+    onClose,
+    onToggle,
+  };
+}
