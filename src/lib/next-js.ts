d3b3aedf26345aab18d1ab5306f5233ed4ddc7b3
jp calvo <calvojp92@gmail.com>
2023-10-15 18:33:10 +0800

    add nextjs lib

---
+++ src/lib/next-js.ts
@@ -0,0 +1,8 @@
+/* eslint-disable no-restricted-imports */
+
+import { styled } from '@/styled-system/jsx';
+import NextImage from 'next/image';
+import NextLink from 'next/link';
+
+export const Link = styled(NextLink);
+export const Image = styled(NextImage);
