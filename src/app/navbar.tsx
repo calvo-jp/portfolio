afe7c8ad4ed54ac0a4f0ac0a5b7db96cd9bdebb3
jp calvo <calvojp92@gmail.com>
2023-10-15 20:18:09 +0800

    init navbar

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,3 +1,51 @@
+import { Button } from '@/lib/button';
+import { Link } from '@/lib/next-js';
+import { Spacer, VisuallyHidden, styled } from '@/styled-system/jsx';
+
 export function Navbar() {
-  return null;
+  return (
+    <styled.header px={10} py={6} display="flex" alignItems="center">
+      <Link href="#">
+        <VisuallyHidden>Home</VisuallyHidden>
+      </Link>
+
+      <Spacer />
+
+      <styled.nav display="flex" alignItems="center" gap={8}>
+        <styled.ul display="flex" alignItems="center" gap={8}>
+          {links.map(({ path, label }, index) => (
+            <styled.li key={path}>
+              <Link href={path} display="flex" alignItems="center" gap={1}>
+                <styled.span fontFamily="mono" color="green" fontSize="xs">
+                  {(index + 1).toString().padStart(2, '0')}.
+                </styled.span>
+                <styled.span fontSize="sm">{label}</styled.span>
+              </Link>
+            </styled.li>
+          ))}
+        </styled.ul>
+
+        <Button size="sm">Resume</Button>
+      </styled.nav>
+    </styled.header>
+  );
 }
+
+const links = [
+  {
+    path: '#about',
+    label: 'About',
+  },
+  {
+    path: '#experience',
+    label: 'Experience',
+  },
+  {
+    path: '#work',
+    label: 'Work',
+  },
+  {
+    path: '#contact',
+    label: 'Contact',
+  },
+];
