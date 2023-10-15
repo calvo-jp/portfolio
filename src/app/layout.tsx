bf2594ba3082f0febcbd2795b60a48aed5038ec5
jp calvo <calvojp92@gmail.com>
2023-10-15 20:12:23 +0800

    change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,7 @@ import '@/assets/styles/globals.css';
 import { cx } from '@/styled-system/css';
 import { styled } from '@/styled-system/jsx';
 import { Metadata } from 'next';
-import { Fira_Code } from 'next/font/google';
-import localFont from 'next/font/local';
+import { JetBrains_Mono, Lato } from 'next/font/google';
 import { PropsWithChildren } from 'react';
 import { Footer } from './footer';
 import { Navbar } from './navbar';
@@ -13,38 +12,14 @@ export const metadata: Metadata = {
   title: 'Calvo JP',
 };
 
-const sans = localFont({
-  src: [
-    {
-      path: '../assets/fonts/CalibreRegular.otf',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../assets/fonts/CalibreMedium.otf',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: '../assets/fonts/CalibreSemibold.otf',
-      weight: '600',
-      style: 'normal',
-    },
-    {
-      path: '../assets/fonts/CalibreBold.otf',
-      weight: '700',
-      style: 'normal',
-    },
-    {
-      path: '../assets/fonts/CalibreBlack.otf',
-      weight: '900',
-      style: 'normal',
-    },
-  ],
+const sans = Lato({
+  weight: ['400'],
+  subsets: ['latin'],
+  display: 'swap',
   variable: '--font-sans',
 });
 
-const mono = Fira_Code({
+const mono = JetBrains_Mono({
   weight: ['400'],
   subsets: ['latin'],
   display: 'swap',
