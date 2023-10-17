7a4271fb07f7d4b6fb16be9f9027ca8b47fedd80
jp calvo <calvojp92@gmail.com>
2023-10-17 19:26:16 +0800

    store in env

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,46 +1,46 @@
 import { Icon } from '@/lib/icon';
-import { HStack, styled } from '@/styled-system/jsx';
+import { Center, HStack, VStack, styled } from '@/styled-system/jsx';
 import { GitBranchIcon, StarIcon } from 'lucide-react';
 
 export function Footer() {
   return (
     <styled.footer>
-      <styled.a
-        href="https://github.com/calvo-jp/calvo-jp"
-        target="_blank"
-        rel="noreferrer noopener"
-        w="fit"
-        mx="auto"
-        fontFamily="mono"
-        fontSize="xs"
-        display="flex"
-        flexDir="column"
-        alignItems="center"
-        justifyContent="center"
-        gap={2}
-        color={{
-          base: 'brand.slate.light',
-          _hover: 'brand.teal',
-        }}
-        transition="colors"
-        transitionDuration="slow"
-      >
-        <styled.div>Designed & Built by John Paul Calvo</styled.div>
-        <HStack gap={4}>
-          <HStack>
-            <Icon asChild>
-              <StarIcon />
-            </Icon>
-            <styled.span>1,001</styled.span>
-          </HStack>
-          <HStack>
-            <Icon asChild>
-              <GitBranchIcon />
-            </Icon>
-            <styled.span>6,205</styled.span>
-          </HStack>
-        </HStack>
-      </styled.a>
+      <Center>
+        <styled.a
+          href={process.env.NEXT_PUBLIC_CREATOR_GITHUB}
+          target="_blank"
+          rel="noreferrer noopener"
+          fontFamily="mono"
+          fontSize="xs"
+          color={{
+            base: 'brand.slate.light',
+            _hover: 'brand.teal',
+          }}
+          transition="colors"
+          transitionDuration="slow"
+        >
+          <VStack gap={2}>
+            <styled.p>
+              Designed & Built by {process.env.NEXT_PUBLIC_CREATOR_NAME}
+            </styled.p>
+
+            <HStack gap={4}>
+              <HStack>
+                <Icon asChild>
+                  <StarIcon />
+                </Icon>
+                <styled.span>1,001</styled.span>
+              </HStack>
+              <HStack>
+                <Icon asChild>
+                  <GitBranchIcon />
+                </Icon>
+                <styled.span>6,205</styled.span>
+              </HStack>
+            </HStack>
+          </VStack>
+        </styled.a>
+      </Center>
     </styled.footer>
   );
 }
