1c97e1b6a05a1ced79e5318c3caae1f2620c1936
jp calvo <calvojp92@gmail.com>
2023-10-20 19:18:42 +0800

    separate recipe

--- src/lib/button.tsx
+++ src/lib/button.tsx
@@ -2,50 +2,48 @@ import { cva } from '@/styled-system/css';
 import { styled } from '@/styled-system/jsx';
 import { ark } from '@ark-ui/react';
 
-export const Button = styled(
-  ark.button,
-  cva({
-    base: {
-      color: 'brand.teal',
-      border: '1px solid token(colors.brand.teal)',
-      cursor: 'pointer',
-      display: 'flex',
-      alignItems: 'center',
-      rounded: 'sm',
-      fontFamily: 'mono',
-      transitionProperty: 'transform',
-      transitionDuration: 'slow',
+const recipe = cva({
+  base: {
+    color: 'brand.teal',
+    border: '1px solid token(colors.brand.teal)',
+    cursor: 'pointer',
+    display: 'flex',
+    alignItems: 'center',
+    rounded: 'sm',
+    fontFamily: 'mono',
+    transitionProperty: 'transform',
+    transitionDuration: 'slow',
 
-      _active: {
-        transform: 'scale(0.95)',
-      },
+    _active: {
+      transform: 'scale(0.95)',
+    },
 
-      _focusVisible: {
-        outline: '2px solid token(colors.brand.slate.light)',
-        outlineOffset: 0.5,
-      },
-    },
-    variants: {
-      size: {
-        sm: {
-          h: 8,
-          px: 4,
-          fontSize: 'xs',
-        },
-        lg: {
-          h: 14,
-          px: 8,
-          fontSize: 'sm',
-        },
-      },
-    },
-    defaultVariants: {
-      size: 'lg',
-    },
-  }),
-  {
-    defaultProps: {
-      type: 'button',
+    _focusVisible: {
+      outline: '2px solid token(colors.brand.slate.light)',
+      outlineOffset: 0.5,
     },
   },
-);
+  variants: {
+    size: {
+      sm: {
+        h: 8,
+        px: 4,
+        fontSize: 'xs',
+      },
+      lg: {
+        h: 14,
+        px: 8,
+        fontSize: 'sm',
+      },
+    },
+  },
+  defaultVariants: {
+    size: 'lg',
+  },
+});
+
+export const Button = styled(ark.button, recipe, {
+  defaultProps: {
+    type: 'button',
+  },
+});
