19f89bb85e11d0ce75cee8b221fe2e4d81cb06ba
jp calvo <calvojp92@gmail.com>
2023-10-21 14:14:10 +0800

    add archive page

--- src/app/archive/page.tsx
+++ src/app/archive/page.tsx
@@ -1,6 +1,12 @@
 import { AUTHOR } from '@/config/author';
-import { styled } from '@/styled-system/jsx';
+import { ExternalLinkIcon, GitBranchIcon } from '@/lib/icons';
+import { Flex, HStack, VisuallyHidden, styled } from '@/styled-system/jsx';
 import { INonFeaturedProject } from '@/types';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: `${AUTHOR.name} | Archive`,
+};
 
 export default function Archive() {
   const items = getItems();
@@ -17,19 +23,88 @@ export default function Archive() {
       <styled.div mt={16}>
         {items.map(([year, projects]) => {
           return (
-            <styled.div key={year}>
-              <styled.div>{year}</styled.div>
+            <Flex
+              key={year}
+              gap={12}
+              mt={{
+                base: 8,
+                _first: 0,
+              }}
+            >
+              <styled.div
+                fontFamily="mono"
+                fontSize="sm"
+                lineHeight="loose"
+                color="brand.accent"
+              >
+                {year}
+              </styled.div>
               <styled.div>
-                {projects.map((project) => (
-                  <styled.div key={project.title}>
-                    <styled.h2>{project.title}</styled.h2>
-                  </styled.div>
+                {projects.map((o) => (
+                  <Item key={o.title} {...o} />
                 ))}
               </styled.div>
-            </styled.div>
+            </Flex>
           );
         })}
       </styled.div>
+    </styled.div>
+  );
+}
+
+function Item(props: INonFeaturedProject) {
+  const { title, description, tags, website, repository } = props;
+
+  return (
+    <styled.div
+      mt={{
+        base: 8,
+        _first: 0,
+      }}
+    >
+      <styled.div>
+        <styled.h2 fontSize="xl" fontWeight="bold" color="brand.slate.lighter">
+          {title}
+        </styled.h2>
+        <styled.p color="brand.slate.light">{description}</styled.p>
+        <styled.ul mt={3} display="flex" gap={3} fontFamily="mono" fontSize="sm">
+          {tags.map((tag) => (
+            <styled.li key={tag}>{tag}</styled.li>
+          ))}
+        </styled.ul>
+      </styled.div>
+
+      <HStack mt={5} gap={4}>
+        {website && (
+          <styled.a
+            href={website}
+            target="_blank"
+            rel="noreferrer noopener"
+            transitionProperty="colors"
+            transitionDuration="slow"
+            _hover={{
+              color: 'brand.accent',
+            }}
+          >
+            <ExternalLinkIcon w={5} h={5} />
+            <VisuallyHidden>Go to website</VisuallyHidden>
+          </styled.a>
+        )}
+
+        <styled.a
+          href={repository}
+          target="_blank"
+          rel="noreferrer noopener"
+          transitionProperty="colors"
+          transitionDuration="slow"
+          _hover={{
+            color: 'brand.accent',
+          }}
+        >
+          <GitBranchIcon w={5} h={5} />
+          <VisuallyHidden>Go to repository</VisuallyHidden>
+        </styled.a>
+      </HStack>
     </styled.div>
   );
 }
