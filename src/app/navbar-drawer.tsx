1fe31c5b6f86d0433c1896e286b5f5bb031ee180
jp calvo <calvojp92@gmail.com>
2023-10-21 20:34:21 +0800

    remove usedisclosure

--- src/app/navbar-drawer.tsx
+++ src/app/navbar-drawer.tsx
@@ -11,129 +11,113 @@ import {
 } from '@/lib/ark-ui';
 import { Button } from '@/lib/button';
 import { MenuIcon, XIcon } from '@/lib/icons';
-import { useDisclosure } from '@/lib/use-disclosure';
+import { Link } from '@/lib/next-js';
 import { Flex, Spacer, styled } from '@/styled-system/jsx';
 import { Portal, Presence } from '@ark-ui/react';
-import { useRouter } from 'next/navigation';
 
 export function NavbarDrawer() {
-  const router = useRouter();
-  const disclosure = useDisclosure();
-
   return (
-    <Dialog
-      open={disclosure.open}
-      onOpenChange={(details) => {
-        if (details.open) {
-          disclosure.onOpen();
-        } else {
-          disclosure.onClose();
-        }
-      }}
-    >
-      <DialogTrigger
-        cursor="pointer"
-        color="brand.white"
-        lg={{
-          display: 'none',
-        }}
-      >
-        <MenuIcon w={8} h={8} />
-      </DialogTrigger>
-
-      <Portal>
-        <Presence present={disclosure.open}>
-          <DialogBackdrop
-            bg="#0a192fbf" // change to "brand.navy/50" once supported
-            pos="fixed"
-            inset={0}
-            zIndex="overlay"
-            backdropFilter="blur(4px)"
-            _open={{
-              animation: 'fade-in 250ms',
+    <Dialog>
+      {({ isOpen, close }) => (
+        <>
+          <DialogTrigger
+            cursor="pointer"
+            color="brand.white"
+            lg={{
+              display: 'none',
             }}
-            _closed={{
-              animation: 'fade-out 250ms',
-            }}
-          />
-        </Presence>
+          >
+            <MenuIcon w={8} h={8} />
+          </DialogTrigger>
 
-        <DialogPositioner>
-          <Presence present={disclosure.open}>
-            <DialogContent
-              p={4}
-              h="full"
-              w="20rem"
-              maxW="full"
-              bg="brand.navy.light"
-              pos="fixed"
-              top={0}
-              right={0}
-              shadow="2xl"
-              zIndex="modal"
-              overflowY="auto"
-              css={{
-                '&::-webkit-scrollbar': {
-                  display: 'none',
-                },
-              }}
-              _open={{
-                animation: 'slide-in-right 250ms',
-              }}
-              _closed={{
-                animation: 'slide-out-right 250ms',
-              }}
-            >
-              <Flex>
-                <Spacer />
-                <DialogCloseTrigger cursor="pointer" color="brand.white">
-                  <XIcon w={8} h={8} />
-                </DialogCloseTrigger>
-              </Flex>
+          <Portal>
+            <Presence present={isOpen}>
+              <DialogBackdrop
+                bg="#0a192fbf" // change to "brand.navy/50" once supported
+                pos="fixed"
+                inset={0}
+                zIndex="overlay"
+                backdropFilter="blur(4px)"
+                _open={{
+                  animation: 'fade-in 250ms',
+                }}
+                _closed={{
+                  animation: 'fade-out 250ms',
+                }}
+              />
+            </Presence>
 
-              <styled.nav py={16}>
-                <styled.ul textAlign="center">
-                  {links.map(({ label, path }, index) => (
-                    <styled.li
-                      key={path}
-                      mb={{
-                        base: 6,
-                        _last: 0,
-                      }}
-                    >
-                      <styled.button
-                        type="button"
-                        py={1}
-                        onClick={() => {
-                          disclosure.onClose();
-                          router.push(path);
-                        }}
+            <DialogPositioner>
+              <Presence present={isOpen}>
+                <DialogContent
+                  p={4}
+                  h="full"
+                  w="20rem"
+                  maxW="full"
+                  bg="brand.navy.light"
+                  pos="fixed"
+                  top={0}
+                  right={0}
+                  shadow="2xl"
+                  zIndex="modal"
+                  overflowY="auto"
+                  css={{
+                    '&::-webkit-scrollbar': {
+                      display: 'none',
+                    },
+                  }}
+                  _open={{
+                    animation: 'slide-in-right 250ms',
+                  }}
+                  _closed={{
+                    animation: 'slide-out-right 250ms',
+                  }}
+                >
+                  <Flex>
+                    <Spacer />
+                    <DialogCloseTrigger cursor="pointer" color="brand.white">
+                      <XIcon w={8} h={8} />
+                    </DialogCloseTrigger>
+                  </Flex>
+
+                  <styled.nav py={16}>
+                    <styled.ul textAlign="center">
+                      {links.map(({ label, path }, index) => (
+                        <styled.li
+                          key={path}
+                          mb={{
+                            base: 6,
+                            _last: 0,
+                          }}
+                        >
+                          <Link py={1} href={path} onClick={close}>
+                            <styled.div fontFamily="mono" color="brand.accent">
+                              {(++index).toString().padStart(2, '0')}.
+                            </styled.div>
+                            <styled.div fontSize="lg" color="brand.slate.lighter">
+                              {label}
+                            </styled.div>
+                          </Link>
+                        </styled.li>
+                      ))}
+                    </styled.ul>
+
+                    <Button w="2/3" mx="auto" mt={12} asChild>
+                      <styled.a
+                        href={AUTHOR.resume}
+                        target="_blank"
+                        rel="noreferrer noopener"
                       >
-                        <styled.div fontFamily="mono" color="brand.accent">
-                          {(++index).toString().padStart(2, '0')}.
-                        </styled.div>
-                        <styled.div fontSize="lg" color="brand.slate.lighter">
-                          {label}
-                        </styled.div>
-                      </styled.button>
-                    </styled.li>
-                  ))}
-                </styled.ul>
-
-                <Button w="2/3" mx="auto" mt={12} asChild>
-                  <styled.a
-                    href={AUTHOR.resume}
-                    target="_blank"
-                    rel="noreferrer noopener"
-                  >
-                    Resume
-                  </styled.a>
-                </Button>
-              </styled.nav>
-            </DialogContent>
-          </Presence>
-        </DialogPositioner>
-      </Portal>
+                        Resume
+                      </styled.a>
+                    </Button>
+                  </styled.nav>
+                </DialogContent>
+              </Presence>
+            </DialogPositioner>
+          </Portal>
+        </>
+      )}
     </Dialog>
   );
 }
