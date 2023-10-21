32c69bdb624b5fdef4e079107b53738c368611d9
jp calvo <calvojp92@gmail.com>
2023-10-21 22:31:56 +0800

    update footer

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -4,22 +4,8 @@ import { Center, styled } from '@/styled-system/jsx';
 export function Footer() {
   return (
     <styled.footer pt={32} pb={8}>
-      <Center>
-        <styled.a
-          href="https://github.com/calvo-jp/calvo-jp"
-          target="_blank"
-          rel="noreferrer noopener"
-          fontFamily="mono"
-          fontSize="xs"
-          color={{
-            base: 'brand.slate.light',
-            _hover: 'brand.accent',
-          }}
-          transitionProperty="colors"
-          transitionDuration="slow"
-        >
-          Made with ❤︎ by {AUTHOR.name}
-        </styled.a>
+      <Center fontFamily="mono" fontSize="sm">
+        &copy; {AUTHOR.name} {new Date().getFullYear()}
       </Center>
     </styled.footer>
   );
