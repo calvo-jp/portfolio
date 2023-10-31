48eb50e799f3ede4c1478ad7d1198019686f14a1
jp calvo <calvojp92@gmail.com>
2023-10-31 21:48:23 +0800

    refactor

--- panda.config.ts
+++ panda.config.ts
@@ -15,19 +15,45 @@ export default defineConfig({
           },
         },
         zIndex: {
-          hide: { value: -1 },
-          auto: { value: 'auto' },
-          base: { value: 0 },
-          docked: { value: 10 },
-          dropdown: { value: 1000 },
-          sticky: { value: 1100 },
-          banner: { value: 1200 },
-          overlay: { value: 1300 },
-          modal: { value: 1400 },
-          popover: { value: 1500 },
-          skipLink: { value: 1600 },
-          toast: { value: 1700 },
-          tooltip: { value: 1800 },
+          hide: {
+            value: -1,
+          },
+          auto: {
+            value: 'auto',
+          },
+          base: {
+            value: 0,
+          },
+          docked: {
+            value: 10,
+          },
+          dropdown: {
+            value: 1000,
+          },
+          sticky: {
+            value: 1100,
+          },
+          banner: {
+            value: 1200,
+          },
+          overlay: {
+            value: 1300,
+          },
+          modal: {
+            value: 1400,
+          },
+          popover: {
+            value: 1500,
+          },
+          skipLink: {
+            value: 1600,
+          },
+          toast: {
+            value: 1700,
+          },
+          tooltip: {
+            value: 1800,
+          },
         },
         spacing: {
           '1/4': { value: '25%' },
@@ -35,24 +61,6 @@ export default defineConfig({
           '1/2': { value: '50%' },
           '2/3': { value: '66.6666666667%' },
           '3/4': { value: '75%' },
-        },
-      },
-      keyframes: {
-        'slide-in-right': {
-          from: { transform: 'translateX(100%)' },
-          to: { transform: 'translateX(0%)' },
-        },
-        'slide-out-right': {
-          from: { transform: 'translateX(0%)' },
-          to: { transform: 'translateX(100%)' },
-        },
-        'fade-in': {
-          from: { opacity: 0 },
-          to: { opacity: 1 },
-        },
-        'fade-out': {
-          from: { opacity: 1 },
-          to: { opacity: 0 },
         },
       },
       semanticTokens: {
@@ -85,6 +93,24 @@ export default defineConfig({
           accent: {
             value: '{colors.violet.500}',
           },
+        },
+      },
+      keyframes: {
+        'slide-in-right': {
+          from: { transform: 'translateX(100%)' },
+          to: { transform: 'translateX(0%)' },
+        },
+        'slide-out-right': {
+          from: { transform: 'translateX(0%)' },
+          to: { transform: 'translateX(100%)' },
+        },
+        'fade-in': {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+        'fade-out': {
+          from: { opacity: 1 },
+          to: { opacity: 0 },
         },
       },
     },
