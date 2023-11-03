d9e872a164a9d85553743af302a5b9f2b705a6bb
jp calvo <calvojp92@gmail.com>
2023-11-03 20:44:01 +0800

    fix 404 in mobile

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,36 +1,58 @@
 import { Button } from '@/components/button';
-import { IconChevronLeft } from '@/components/icons';
 import { Link } from '@/components/link';
 import { Box, styled } from '@/styled-system/jsx';
 
 export default function NotFound() {
   return (
-    <Box px={8} pt={40} pb={32} textAlign="center">
+    <Box
+      px={8}
+      pt={{
+        base: 48,
+        lg: 40,
+      }}
+      pb={{
+        base: 40,
+        lg: 32,
+      }}
+      textAlign="center"
+    >
       <styled.h1
         fontFamily="mono"
-        fontSize="9xl"
-        fontWeight="bold"
+        fontSize={{
+          base: '8xl',
+          lg: '9xl',
+        }}
+        fontWeight="black"
         lineHeight="none"
         color="fg.accent"
       >
         404
       </styled.h1>
-      <styled.h2 fontSize="3xl" lineHeight="none" color="fg.strong">
+      <styled.h2
+        fontSize={{
+          base: '2xl',
+          lg: '3xl',
+        }}
+        lineHeight="none"
+        color="fg.strong"
+      >
         Page Not Found
       </styled.h2>
       <Button asChild>
         <Link
           href="/"
-          w={40}
-          mt={16}
+          w={48}
+          mt={{
+            base: 12,
+            lg: 16,
+          }}
           mx="auto"
-          gap={2}
+          gap={3}
           display="flex"
           alignItems="center"
           justifyContent="center"
         >
-          <IconChevronLeft />
-          <styled.span>Go Home</styled.span>
+          Go Home
         </Link>
       </Button>
     </Box>
