b2516efda88cf9910f0b51b559cf14680b5b0a4f
jp calvo <calvojp92@gmail.com>
2023-11-03 16:48:03 +0800

    use parser for rendering raw html

--- src/components/raw-html.tsx
+++ src/components/raw-html.tsx
@@ -1,6 +1,8 @@
+import { cva, cx } from '@/styled-system/css';
 import { styled } from '@/styled-system/jsx';
 import { Assign, HTMLStyledProps } from '@/styled-system/types';
 import { HTMLArkProps, ark } from '@ark-ui/react';
+import parse from 'html-react-parser';
 import { forwardRef } from 'react';
 
 interface StyledArkDivProps extends Assign<HTMLArkProps<'div'>, HTMLStyledProps<'div'>> {}
@@ -8,53 +10,51 @@ const StyledArkDiv = styled(ark.div);
 
 interface RawHtmlBaseProps {
   children: string;
-  dangerouslySetInnerHTML?: never;
 }
 
 interface RawHtmlProps extends Assign<StyledArkDivProps, RawHtmlBaseProps> {}
 
 export const RawHtml = forwardRef<HTMLDivElement, RawHtmlProps>(
   function RawHtml(props, ref) {
-    const { children: __html, css, ...others } = props;
+    const { children, className, ...others } = props;
 
     return (
-      <StyledArkDiv
-        ref={ref}
-        css={{
-          hyphens: 'auto',
-
-          '& a': {
-            color: 'fg.accent',
-            textUnderlineOffset: '0.35rem',
-            _hover: {
-              textDecoration: 'underline',
-            },
-          },
-
-          '& li': {
-            mt: 2,
-
-            _first: {
-              mt: 0,
-            },
-          },
-
-          '& p': {
-            mt: {
-              base: 4,
-              lg: 5,
-            },
-
-            _first: {
-              mt: 0,
-            },
-          },
-
-          ...css,
-        }}
-        dangerouslySetInnerHTML={{ __html }}
-        {...others}
-      />
+      <StyledArkDiv ref={ref} className={cx(rawHtmlRecipe(), className)} {...others}>
+        {parse(children)}
+      </StyledArkDiv>
     );
   },
 );
+
+const rawHtmlRecipe = cva({
+  base: {
+    hyphens: 'auto',
+
+    '& a': {
+      color: 'fg.accent',
+      textUnderlineOffset: '0.35rem',
+      _hover: {
+        textDecoration: 'underline',
+      },
+    },
+
+    '& li': {
+      mt: 2,
+
+      _first: {
+        mt: 0,
+      },
+    },
+
+    '& p': {
+      mt: {
+        base: 4,
+        lg: 5,
+      },
+
+      _first: {
+        mt: 0,
+      },
+    },
+  },
+});
