971fca46123fcfc1678a414f25563672ea67d4dd
jp calvo <calvojp92@gmail.com>
2023-11-06 17:52:23 +0800

    refactor provider

--- packages/v1/src/app/providers.tsx
+++ packages/v1/src/app/providers.tsx
@@ -1,11 +1,22 @@
 'use client';
 
+import {Theme} from '@/utils/types';
 import {ThemeProvider} from 'next-themes';
 import {PropsWithChildren} from 'react';
 
 export function Providers(props: PropsWithChildren) {
 	return (
-		<ThemeProvider attribute="class" storageKey="theme" defaultTheme="system">
+		<ThemeProvider
+			attribute="class"
+			storageKey="theme"
+			defaultTheme={Theme.System}
+			themes={Object.values(Theme)}
+			value={{
+				dark: Theme.Dark,
+				light: Theme.Light,
+				system: Theme.System,
+			}}
+		>
 			{props.children}
 		</ThemeProvider>
 	);
