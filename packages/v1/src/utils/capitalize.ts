e8acdefe23d4f9c8df0956b7667b4cf5e530e815
jp calvo <calvojp92@gmail.com>
2023-11-11 11:30:25 +0800

    optimize capitalize

--- packages/v1/src/utils/capitalize.ts
+++ packages/v1/src/utils/capitalize.ts
@@ -2,11 +2,5 @@ export function capitalize(subject: string) {
 	return subject
 		.trim()
 		.toLowerCase()
-		.split(/\s/)
-		.map((word) => {
-			return word.length === 1
-				? word.toUpperCase()
-				: word.charAt(0).toUpperCase() + word.substring(1);
-		})
-		.join(' ');
+		.replace(/\b\w/g, (char) => char.toUpperCase());
 }
