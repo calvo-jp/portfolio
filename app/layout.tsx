0b5ef6dd3e77ca34694b29f5db41dbb12a6f4107
jp calvo <calvojp92@gmail.com>
2024-02-18 18:00:05 +0800

    fix metadata base

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export async function generateMetadata(): Promise<Metadata> {
 			template: `${title} - %s`,
 		},
 		description,
-		metadataBase: new URL('https://calvo-jp.vercel.app'),
+		metadataBase: new URL('https://calvojp.vercel.app'),
 		keywords: [
 			'Porfolio',
 			'Dark Mode',
