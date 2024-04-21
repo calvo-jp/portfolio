159c5e8b1d689bda6f0bd6ce525684cc53f46a53
jp calvo <calvojp92@gmail.com>
2024-04-21 16:29:28 +0800

    disabled typescript and eslint during builds

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 
 /** @type {import('next').NextConfig} */
 module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   experimental: {
     serverMinification: true,
     optimizePackageImports: ['@untitled-theme/icons-react'],
